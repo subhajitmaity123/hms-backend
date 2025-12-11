8663d3ce03646f91aaf22d6582c91e59245a62e4
subhajitmaity123 <109075899+subhajitmaity123@users.noreply.github.com>
2025-12-11 11:32:43 +0530

    changed the cors

--- app.js
+++ app.js
@@ -15,9 +15,8 @@ config({ path: "./config.env" });
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
-    method: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
 
